bf93e7ad96cbf585f69f98e6b910c45acf5e28b7
Packard <packardgilson@gmail.com>
2016-01-14 18:04:06 -0500

    BT connection update
    Added an icon that will pop up with a wrist vibrate when BT is disconnected.

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_date_layer;
 static GFont s_time_font, s_date_font;
-static BitmapLayer *s_background_layer;
-static GBitmap *s_background_bitmap;
+static BitmapLayer *s_background_layer, *s_bt_icon_layer;
+static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
 
 static void update_time() {
   //Get tm struct
@@ -14,10 +14,10 @@ static void update_time() {
   //Write current hours/min into buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+                                          "%h:%M" : "%I:%M", tick_time);
   // Write date into buffer
   static char s_date_buffer[16];
-  strftime(s_date_buffer, sizeof(s_date_buffer), "%a,%b %d", tick_time);
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a.%b %d", tick_time);
   
   //Display this time & date on text layer
   text_layer_set_text(s_time_layer, s_buffer);
@@ -28,6 +28,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   update_time();
 }
 
+static void bluetooth_callback(bool connected) {
+  //show icon if disconnected
+  layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+  
+  if(!connected){
+    // Vibrate alert!
+    vibes_double_pulse();
+  }
+}
+
 static void main_window_load(Window *window){
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
@@ -35,14 +45,19 @@ static void main_window_load(Window *window){
 
   // create GBitmap
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ATOM);
+  s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_ICON);
   
   // Create BitmapLayer to display the GBitmap
   s_background_layer = bitmap_layer_create(bounds);
+  s_bt_icon_layer = bitmap_layer_create(GRect(124, 144, 22, 24));
   
   // Set the bitmap onto the layer and add to the window
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+  
   //bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bt_icon_layer));
   
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
@@ -69,6 +84,9 @@ static void main_window_load(Window *window){
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
   
+  // Show correct state of BT connection from the start
+  bluetooth_callback(connection_service_peek_pebble_app_connection());
+  
   // Trying to vertically align text layer
 //   GRect frame = layer_get_frame(text_layer_get_layer(s_time_layer));
 //   GSize content_size = text_layer_get_content_size(s_time_layer);
@@ -84,8 +102,10 @@ static void main_window_unload(Window *window){
   fonts_unload_custom_font(s_date_font);
   //Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
+  gbitmap_destroy(s_bt_icon_bitmap);
   //Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
+  bitmap_layer_destroy(s_bt_icon_layer);
 }
 
 static void init() {
@@ -108,6 +128,12 @@ static void init() {
   
   //Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  //Register for Bluetooth connection updates
+  connection_service_subscribe((ConnectionHandlers) {
+    .pebble_app_connection_handler = bluetooth_callback
+  });
+  
 }
 
 static void deinit() {
