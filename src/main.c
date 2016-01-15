3aa7db761756487477d06272fc7f723ca4e34329
Packard <packardgilson@gmail.com>
2016-01-15 13:26:51 -0500

    Added Weather updates
    via OpenWeatherMap api

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <pebble.h>
 
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
 static Window *s_main_window;
-static TextLayer *s_time_layer, *s_date_layer;
-static GFont s_time_font, s_date_font;
+static TextLayer *s_time_layer, *s_date_layer, *s_weather_layer;
+static GFont s_time_font, s_date_font, s_weather_font;
 static BitmapLayer *s_background_layer, *s_bt_icon_layer;
 static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
 
@@ -26,6 +29,19 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   update_time();
+  
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 static void bluetooth_callback(bool connected) {
@@ -61,12 +77,14 @@ static void main_window_load(Window *window){
   
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(-5, PBL_IF_ROUND_ELSE(58, 99), bounds.size.w, 50));
-  s_date_layer = text_layer_create(GRect(0, 144, 144, 30));
+      GRect(-5, PBL_IF_ROUND_ELSE(58, 66), bounds.size.w, 50));
+  s_date_layer = text_layer_create(GRect(0, 120, 144, 30));
+  s_weather_layer = text_layer_create(GRect(0, 148, bounds.size.w, 20));
   
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_AKASHI_35));
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_AKASHI_19));
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_AKASHI_15));
 
   // Improve the layout to be more like a watchface
   // Time
@@ -79,33 +97,70 @@ static void main_window_load(Window *window){
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorBlack);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  // Weather
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
   
   // Show correct state of BT connection from the start
   bluetooth_callback(connection_service_peek_pebble_app_connection());
-  
-  // Trying to vertically align text layer
-//   GRect frame = layer_get_frame(text_layer_get_layer(s_time_layer));
-//   GSize content_size = text_layer_get_content_size(s_time_layer);
-//   layer_set_frame(text_layer_get_layer(s_time_layer), GRect(frame.origin.x, frame.origin.y + (frame.size.h - content_size.h) / 2, frame.size.w, content_size.h));
 }
 
 static void main_window_unload(Window *window){
   //Destroy text layer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_weather_layer);
   //Unload GFont
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_date_font);
+  fonts_unload_custom_font(s_weather_font);
   //Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
   gbitmap_destroy(s_bt_icon_bitmap);
   //Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
   bitmap_layer_destroy(s_bt_icon_layer);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context){
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%d F", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+  }
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context){
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context){
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context){
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init() {
@@ -133,6 +188,15 @@ static void init() {
   connection_service_subscribe((ConnectionHandlers) {
     .pebble_app_connection_handler = bluetooth_callback
   });
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  //Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
   
 }
 
